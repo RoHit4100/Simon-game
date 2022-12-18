f3c174bb9cb96f90e885b28f482ad171971d80d1
hackur333 <70364267+RoHit4100@users.noreply.github.com>
2022-12-18 21:39:29 +0530

    repo is created
    addition files are uploaded

---
+++ game.js
@@ -0,0 +1,21 @@
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+nextSequence(buttonColors, gamePattern);
+
+function nextSequence(buttonColors, gamePattern) {
+  var randomNumber = Math.floor(Math.random() * 3) + 1;
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
