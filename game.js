ce30408ec574252d6514a5454f22245ed69ba64d
hackur333 <70364267+RoHit4100@users.noreply.github.com>
2022-12-19 20:53:05 +0530

    project is completed
    This is the first project in which I used my JQuery knowledge

--- game.js
+++ game.js
@@ -1,12 +1,59 @@
-var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
-nextSequence(buttonColors, gamePattern);
+var gamePattern = [];
+var userClickPattern = [];
+var level = 0;
+var started = false;
 
-function nextSequence(buttonColors, gamePattern) {
+//To start the game and keep track of the game and also change heading
+$(document).keypress(function (e) {
+  if (!started) {
+    $("h1").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+//When button is click sequence will be append into the array
+$(".btn").on("click", function () {
+  var userChosenColor = $(this).attr("id");
+  userClickPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickPattern.length - 1);
+});
+
+//To check the ans
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickPattern[currentLevel]) {
+    if (userClickPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
+
+//To generate new sequence of the game
+function nextSequence() {
+  userClickPattern = [];
+  level++;
+  $("h1").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 3) + 1;
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
+  //for animation of fade in fade out
   $("#" + randomChosenColor)
     .fadeIn(100)
     .fadeOut(100)
@@ -15,6 +62,22 @@ function nextSequence(buttonColors, gamePattern) {
   playSound(randomChosenColor);
 }
 
+//To start over the game if conditions dose'nt matches
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+//To add animation after button is clicked
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+//To play the sound
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
